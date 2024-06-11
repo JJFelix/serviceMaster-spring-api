e765d56261871e964970f64c7b918d489c3a24db
JJFelix <felixomwenga441@gmail.com>
2024-06-11 19:16:23 +0300

    Backend api set up

---
+++ src/test/java/com/felix/serviceMaster/ServiceMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felix.serviceMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
