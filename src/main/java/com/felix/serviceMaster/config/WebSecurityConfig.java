fffa32c92874a3d6f2e04c851756ea773f78e998
JJFelix <felixomwenga441@gmail.com>
2024-06-14 15:49:17 +0300

    Fri midday

---
+++ src/main/java/com/felix/serviceMaster/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.felix.serviceMaster.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.felix.serviceMaster.services.jwt.JwtRequestFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private JwtRequestFilter requestFilter;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { // was SecurityFilterChain before changed to HttpSecurity
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/authenticate", "/company/sign-up", "/client-sign-up", "/ads", "/search/{service}")
+                .permitAll().anyRequest().authenticated()
+            )
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
+
+            return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+}
